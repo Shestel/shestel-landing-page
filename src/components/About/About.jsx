3f14272f02e205292b780b2d45c770765d732553
jhimmyofficial <olufemijhimmy@gmail.com>
2022-10-10 15:39:57 +0100

    new

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./About.css";
 // import imageabout from "../../assets/images/image-about.webp";
 // import image16 from "../../assets/images/image16.webp";
 import check from "../../assets/images/check-mark.png";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 const About = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <section className="about__container">
       <div className="about__content">
@@ -17,10 +21,17 @@ const About = () => {
           content recommendations.
         </p>
 
-        <div className="about__features--content">
+        <div className="about__features--content"  data-aos="fade-up"
+              data-aos-offset="250"
+              data-aos-delay="100"
+              data-aos-duration="1000"
+              data-aos-easing="ease">
           <h2>Product Features</h2>
           <div className="about__reasons">
-            <div className="about__reasons--child">
+            <div
+              className="about__reasons--child"
+             
+            >
               <img src={check} alt="icon" />
               <p>Personalized User Profile</p>
             </div>

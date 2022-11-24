6741736fddfd485cf8e18697e171341191c5668a
jhimmyofficial <olufemijhimmy@gmail.com>
2022-11-24 08:56:04 +0100

    added carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,32 +1,13 @@
 import React from "react";
 import "./Carousel.css";
 import Slider from "react-slick";
-import image1 from "../../assets/images/1.jpg";
-import image2 from "../../assets/images/2.jpg";
-import image3 from "../../assets/images/3.jpg";
-import image4 from "../../assets/images/4.jpg";
-import image5 from "../../assets/images/5.jpg";
-import image6 from "../../assets/images/6.jpg";
-import image7 from "../../assets/images/7.jpg";
-import image8 from "../../assets/images/8.jpg";
-import image9 from "../../assets/images/9.jpg";
-import image10 from "../../assets/images/10.jpg";
-import image11 from "../../assets/images/11.jpg";
-import image12 from "../../assets/images/12.jpg";
+import image1 from "../../assets/images/company1.webp"
+import image2 from "../../assets/images/company2.webp"
+import image3 from "../../assets/images/company3.webp"
+import image4 from "../../assets/images/company4.webp"
+import image5 from "../../assets/images/company5.webp"
+import image6 from "../../assets/images/company6.webp"
 
-// import jigsaw from "../../assets/images/jigsaw.png";
-// import money from "../../assets/images/money.png";
-// import chat from "../../assets/images/chat.png";
-// import netflix from '../../assets/images/netflix.webp'
-// import apple from '../../assets/images/apple.webp'
-// import peacock from '../../assets/images/peacock.webp'
-// import amazon from '../../assets/images/prime.webp'
-// import youtube from '../../assets/images/youtube.webp'
-// import hbo from '../../assets/images/hbo.webp'
-// import hulu from '../../assets/images/hulu.webp'
-// import tubi from '../../assets/images/tubi.webp'
-// import paramount from '../../assets/images/paramount.webp'
-// import disney from '../../assets/images/disney.webp'
 
 const Carousel = () => {
   const settings = {
@@ -76,13 +57,22 @@ const Carousel = () => {
       <Slider {...settings}>
 
         <div className="carousel--content">
-          <img src={image1} alt="" />
+          <img src={image1} alt="About-Shestel" />
         </div>
         <div className="carousel--content">
-          <img src={image2} alt="" />
+          <img src={image2} alt="About-Shestel" />
         </div>
         <div className="carousel--content">
-          <img src={image3} alt="" />
+          <img src={image3} alt="About-Shestel" />
+        </div>
+        <div className="carousel--content">
+          <img src={image4} alt="About-Shestel" />
+        </div>
+        <div className="carousel--content">
+          <img src={image5} alt="About-Shestel" />
+        </div>
+        <div className="carousel--content">
+          <img src={image6} alt="About-Shestel" />
         </div>
 
         {/* <div className="carousel--content">
@@ -97,25 +87,6 @@ const Carousel = () => {
           <h2>CONNECT</h2>
         </div> */}
 
-        {/* <img src={netflix} alt="Client Logo" />
-
-        <img src={apple} alt="Client Logo" />
-
-        <img src={hulu} alt="Client Logo" />
-
-        <img src={amazon} alt="Client Logo" className="carousel-big"/>
-
-        <img src={youtube} alt="Client Logo" className="carousel-big"/>
-
-        <img src={hbo} alt="Client Logo" />
-
-        <img src={disney} alt="Client Logo" />
-
-        <img src={tubi} alt="Client Logo" />
-
-        <img src={paramount} alt="Client Logo" />
-
-        <img src={peacock} alt="Client Logo" /> */}
       </Slider>
     </div>
   );
